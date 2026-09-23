4adf19e73f5f3ff3dfb2921f50d24264617deb10
mikeeezzyy <michael.lucas113@gmail.com>
2026-09-23 19:04:47 -0400

    week 2 rankings - no commentary

--- src/lib/PowerRankings/powerRankingsData.js
+++ src/lib/PowerRankings/powerRankingsData.js
@@ -33,73 +33,73 @@ export const POWER_RANKINGS = {
 				rank: 1,
 				previousRank: null,
 				team: 'connorbrannick',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 2,
 				previousRank: null,
 				team: 'The Fighting Lil Jonahs',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 3,
 				previousRank: null,
 				team: 'Baumy1220',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 4,
 				previousRank: null,
 				team: "The Toledo Whiteskins",
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 5,
 				previousRank: null,
 				team: 'Tax Evasion',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 6,
 				previousRank: null,
 				team: 'thoy113',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 7,
 				previousRank: null,
 				team: '#1 Woman Respector',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 8,
 				previousRank: null,
 				team: 'Slim Pickens',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 9,
 				previousRank: null,
 				team: 'Curious George',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 10,
 				previousRank: null,
 				team: 'The Dykes of Hazard',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 11,
 				previousRank: null,
 				team: 'Kupp My Balls',
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			},
 			{
 				rank: 12,
 				previousRank: null,
 				team: "Ja'Marr Chase Little Kids",
-				commentary: 'Write your Week 1 commentary here.'
+				commentary: 'Commish sucks, no week 1 commentary'
 			}
 		]
 	},
@@ -109,37 +109,37 @@ export const POWER_RANKINGS = {
 			{
 				rank: 1,
 				previousRank: 1,
-				team: 'Tax Evasion',
+				team: 'connorbrannick',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 2,
 				previousRank: 2,
-				team: '',
+				team: 'Tax Evasion',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 3,
 				previousRank: 3,
-				team: 'Kupp My Balls',
+				team: 'The Toledo Whiteskins',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 4,
 				previousRank: 4,
-				team: "Ja'Marr Chase Little Kids",
+				team: 'Baumy1220',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 5,
 				previousRank: 5,
-				team: 'The Toledo Whiteskins',
+				team: 'Curious George',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 6,
 				previousRank: 6,
-				team: 'The Dykes of Hazard',
+				team: 'The Fighting Lil Jonahs',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
@@ -151,31 +151,31 @@ export const POWER_RANKINGS = {
 			{
 				rank: 8,
 				previousRank: 8,
-				team: 'Slim Pickens',
+				team: 'The Dykes of Hazard',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 9,
 				previousRank: 9,
-				team: 'connorbrannick',
+				team: 'Slim Pickens',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 10,
 				previousRank: 10,
-				team: 'The Fighting Lil Jonahs',
+				team: 'Thoy113',
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 11,
 				previousRank: 11,
-				team: 'thoy113',
+				team: "Ja’Marr Chase Little Kids",
 				commentary: 'Write your Week 2 commentary here.'
 			},
 			{
 				rank: 12,
 				previousRank: 12,
-				team: 'Baumy1220',
+				team: 'Kupp My Balls',
 				commentary: 'Write your Week 2 commentary here.'
 			}
 		]
